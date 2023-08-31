854728f98b530a3dc5c85529dcff76513fac94d2
Juyoun Kim <juyoun.kim@mobilus.co.jp>
2023-08-31 17:35:20 +0900

    changed mongodb driver to 5.8.1 for mongo6.0 support & update adapter for it & Update README.md
    added append collection handling
    
    added new config for mongodb options

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
-FROM nodesource/node:4.2
+FROM node:16-alpine3.17
+
+RUN apk add --no-cache make
+
+WORKDIR /usr/src/app
 
 ADD package.json package.json
-RUN npm install
+ADD yarn.lock yarn.lock
+RUN yarn
 ADD . .
 
-CMD ["npm","test"]
+CMD ["yarn","test"]
--- test/unit/adapter.native.js
+++ test/unit/adapter.native.js
@@ -1,45 +1,37 @@
-var Adapter = require('../../lib/adapter'),
-    Config = require('../support/config'),
-    Fixture = require('../support/fixture'),
-    assert = require('assert'),
-    async = require('async');
+var Adapter = require("../../lib/adapter"),
+  Config = require("../support/config"),
+  Fixture = require("../support/fixture"),
+  assert = require("assert"),
+  async = require("async");
 
-describe('adapter', function() {
-
-  before(function(done) {
+describe("adapter", function () {
+  before(function (done) {
     var Schema;
 
     var connection = Config;
-    connection.identity = 'test';
+    connection.identity = "test";
 
-    var collection = { identity: 'foobar', definition: Fixture };
-    collection.definition.connection = 'test';
-
-    Adapter.registerConnection(connection, { 'foobar': collection }, done);
+    var collection = { identity: "foobar", definition: Fixture };
+    collection.definition.connection = "test";
+    Adapter.registerConnection(connection, { foobar: collection }, done);
   });
 
-
-  describe('.native()', function() {
-
-    it('should allow direct access to the collection object', function(done) {
-
-      Adapter.native('test', 'foobar', function(err, collection) {
+  describe(".native()", function () {
+    it("should allow direct access to the collection object", function (done) {
+      Adapter.native("test", "foobar", async function (err, collection) {
         assert(!err);
 
         // Attempt to insert a document
-        collection.insert({hello: 'world'}, {w:1}, function(err, objects) {
-          assert(!err);
-
-          // check that the record was actually inserted
-          collection.findOne({ hello: 'world' }, function(err, doc) {
-            assert(!err);
-            assert(doc.hello === 'world');
-
-            done(err);
-          });
-        });
+        try {
+          await collection.insertOne({ hello: "world" }, { w: 1 });
+          const doc = await collection.findOne({ hello: "world" });
+          assert(doc.hello === "world");
+          done();
+        } catch (e) {
+          assert(!e);
+          done(err);
+        }
       });
     });
   });
-
 });
